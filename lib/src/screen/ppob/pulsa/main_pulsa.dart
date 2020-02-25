da07b5f60f6bb787376e0e3d54fa9b52168af279
amatoxin404 <cyberregin@gmail.com>
2020-02-25 12:27:45 +0700

    detail update

---
+++ lib/src/screen/ppob/pulsa/main_pulsa.dart
@@ -0,0 +1,52 @@
+import 'package:ansor_build/src/screen/ppob/pulsa/appbar_copy.dart';
+import 'package:flutter/material.dart';
+
+
+class TabBarDemo extends StatefulWidget {
+  @override
+  _TabBarDemoState createState() => _TabBarDemoState();
+}
+
+class _TabBarDemoState extends State<TabBarDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('data'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top:80.0),
+             child: Column(
+               children: <Widget>[
+                 Expanded(child: BodyTabBar1()),
+               ],
+             ),
+          ),
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.only(),
+              child: new Column(
+                children: <Widget>[
+                  _buildButtonsRow()
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+    Widget _buildButtonsRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 125.0,
+        width: 450.0,
+        color: Colors.greenAccent,
+        child: Text('INI IKLAN LAGI`'),
+      ),
+    );
+  }
+}
