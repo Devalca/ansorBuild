158f1984fe3959e0b24ca90f451b96e4438c0d4a
amatoxin404 <cyberregin@gmail.com>
2020-04-07 17:00:22 +0700

    fix testing

--- lib/src/screen/katalog/katalog_detail.dart
+++ lib/src/screen/katalog/katalog_detail.dart
@@ -9,6 +9,7 @@ class _DetailKatalogPageState extends State<DetailKatalogPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Detail "Nama Barang"'),
       ),
